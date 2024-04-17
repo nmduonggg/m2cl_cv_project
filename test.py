91ca49dd7a185fb9d96d5af8872a029f65912788
KienTranDSAI <kientran.dsai@gmail.com>
2024-04-17 17:53:32 +0700

    Add accuracy on test.py

--- test.py
+++ test.py
@@ -8,10 +8,14 @@ from model import M2CL18
 def do_test(network, dataloader):
     network.eval()
     test_loss = 0
+    true_pred = 0
     for x,y in dataloader:
         preds, conv_act = network(x)
         loss = F.cross_entropy(preds, y)
         test_loss += loss
         test_loss = test_loss / len(dataloader.dataset)
+        y_pred = torch.argmax(preds, 1)
+        true_pred += torch.sum(y_pred == y).item()
     print(f"Test loss on new domain: {test_loss}")
+    print(f"Accuracy on new domain is: {true_pred/len(dataloader.dataset)}")
     return test_loss
